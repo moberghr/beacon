592241394d2dd051a34cf7b02d12bd91be6079ac
Mario Svetec <mario.svetec1@gmail.com>
2023-05-25 12:45:21 +0200

    get projects  command

--- Semantico.Api/Handlers/Projects/GetProjectsQuery.cs
+++ Semantico.Api/Handlers/Projects/GetProjectsQuery.cs
@@ -1,6 +1,26 @@
-﻿namespace Semantico.Api.Handlers.Projects;
+﻿using Semantico.Api.Data;
+
+namespace Semantico.Api.Handlers.Projects;
 
 public class GetProjectsQuery
 {
+    private readonly SemanticoContext _context;
+
+    public GetProjectsQuery(SemanticoContext context)
+    {
+        _context = context;
+    }
+
+}
+
+public class GetProjectsRequest
+{ }
+
+public class GetProjectsResponse
+{ }
+
+public class GetProjectsResponseListData
+{
+
 }
 
