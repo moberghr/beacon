e57a510bf0b2b8a2d2c9c785f34565d5362131f2
Mario Svetec <mario.svetec1@gmail.com>
2023-05-25 12:53:23 +0200

    update

--- Semantico.Api/Handlers/Projects/GetProjectsQuery.cs
+++ Semantico.Api/Handlers/Projects/GetProjectsQuery.cs
@@ -1,8 +1,10 @@
-﻿using Semantico.Api.Data;
+﻿using MediatR;
+using Semantico.Api.Data;
+using Semantico.Api.Data.Entities;
 
 namespace Semantico.Api.Handlers.Projects;
 
-public class GetProjectsQuery
+public class GetProjectsQuery : IRequestHandler<GetProjectsRequest, GetProjectsResponse>
 {
     private readonly SemanticoContext _context;
 
@@ -11,16 +13,32 @@ public class GetProjectsQuery
         _context = context;
     }
 
+    public async Task<GetProjectsResponse> Handle(GetProjectsRequest request, CancellationToken cancellationToken)
+    {
+        return new GetProjectsResponse
+        {
+            Projects =
+        };
+    }
+
 }
 
-public class GetProjectsRequest
-{ }
+public class GetProjectsRequest : IRequest<GetProjectsResponse>
+{
+    public int?
+}
 
 public class GetProjectsResponse
-{ }
+{
+    public required List<GetProjectsResponseListData> Projects { get; set; }
+}
 
 public class GetProjectsResponseListData
 {
+    public required string Name { get; set; }
 
+    public required string ConnectionString { get; set; }
+
+    public List<Query> Queries { get; set; } = new();
 }
 
