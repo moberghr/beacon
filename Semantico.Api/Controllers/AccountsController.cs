51fdcab084d0788de259c10bcbd9fee42d6ecb2a
Mario Svetec <mario.svetec1@gmail.com>
2023-07-10 15:55:17 +0200

    handlers

---
+++ Semantico.Api/Controllers/AccountsController.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Semantico.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AccountsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public AccountsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task CreateUserAccount()
+    {
+
+    }
+
+    [HttpPost]
+    public async Task RemoveUserAccount()
+    { }
+
+}
+
---
+++ Semantico.Api/Handlers/Accounts/DeleteAccountCommand.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Semantico.Api.Data;
+
+namespace Semantico.Api.Handlers.Accounts;
+
+public class DeleteAccountCommand : IRequestHandler<DeleteAccountRequest, DeleteAccountResponse>
+{
+    private readonly SemanticoContext _context;
+
+    public DeleteAccountCommand(SemanticoContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<CreateAccountResponse> Handle(DeleteAccountRequest request, CancellationToken cancellationToken)
+    {
+        var username = await _context.Accounts
+            .Where(x => x.Id == request.AccountId)
+            .SingleAsync();
+
+        return new();
+    }
+}
+
+public class DeleteAccountRequest : IRequest<DeleteAccountResponse>
+{
+    public required int AccountId { get; set; }
+}
+
+public class DeleteAccountResponse
+{
+}
