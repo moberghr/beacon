b6b146ea564f1294169d3e1139808cd2c77ab990
Domagoj Medo <domagoj.medo@moberg.hr>
2023-07-10 10:42:34 +0200

    namespace fix

--- Semantico.Api/Web/BasicAuthenticationHandler.cs
+++ Semantico.Api/Web/BasicAuthenticationHandler.cs
@@ -4,57 +4,56 @@ using System.Text.Encodings.Web;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Options;
 
-namespace Semantico.Api.Web
+namespace Semantico.Api.Web;
+
+public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
 {
-    public class BasicAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+        : base(options, logger, encoder, clock)
     {
-        public BasicAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
-            : base(options, logger, encoder, clock)
-        {
-        }
+    }
 
-        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        try
         {
-            try
+            var authHeader = Request.Headers.Authorization.ToString() ?? string.Empty;
+
+            if (!authHeader.StartsWith("basic", StringComparison.OrdinalIgnoreCase))
             {
-                var authHeader = Request.Headers.Authorization.ToString() ?? string.Empty;
-
-                if (!authHeader.StartsWith("basic", StringComparison.OrdinalIgnoreCase))
-                {
-                    return AuthenticateResult.Fail("Missing Authorization Header.");
-                }
-
-                var token = authHeader["Basic ".Length..].Trim();
-                var credentialString = Encoding.UTF8.GetString(Convert.FromBase64String(token));
-                var credentials = credentialString.Split(':');
-
-                if (credentials[0] == "moberg" && credentials[1] == "3Semantico6#")
-                {
-                    var claims = new List<Claim>
-                    {
-                        new Claim("name", credentials[0]),
-                        new Claim(ClaimTypes.Role, "Admin"),
-                    };
-
-                    var identity = new ClaimsIdentity(claims, "Basic");
-                    var claimsPrincipal = new ClaimsPrincipal(identity);
-
-                    return AuthenticateResult.Success(new AuthenticationTicket(claimsPrincipal, Scheme.Name));
-                }
-
-                return AuthenticateResult.Fail("Invlid credentials.");
+                return AuthenticateResult.Fail("Missing Authorization Header.");
             }
-            catch (Exception e)
+
+            var token = authHeader["Basic ".Length..].Trim();
+            var credentialString = Encoding.UTF8.GetString(Convert.FromBase64String(token));
+            var credentials = credentialString.Split(':');
+
+            if (credentials[0] == "moberg" && credentials[1] == "3Semantico6#")
             {
-                return AuthenticateResult.Fail(e.Message);
-            }
-        }
+                var claims = new List<Claim>
+                {
+                    new Claim("name", credentials[0]),
+                    new Claim(ClaimTypes.Role, "Admin"),
+                };
 
-        protected override Task HandleChallengeAsync(AuthenticationProperties properties)
-        {
-            Response.StatusCode = 401;
-            Response.Headers.Add("WWW-Authenticate", "Basic realm=\"semantico.com\"");
-            return base.HandleChallengeAsync(properties);
+                var identity = new ClaimsIdentity(claims, "Basic");
+                var claimsPrincipal = new ClaimsPrincipal(identity);
+
+                return AuthenticateResult.Success(new AuthenticationTicket(claimsPrincipal, Scheme.Name));
+            }
+
+            return AuthenticateResult.Fail("Invlid credentials.");
         }
+        catch (Exception e)
+        {
+            return AuthenticateResult.Fail(e.Message);
+        }
+    }
+
+    protected override Task HandleChallengeAsync(AuthenticationProperties properties)
+    {
+        Response.StatusCode = 401;
+        Response.Headers.Add("WWW-Authenticate", "Basic realm=\"semantico.com\"");
+        return base.HandleChallengeAsync(properties);
     }
 }
