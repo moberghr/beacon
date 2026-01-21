8190589a42a7462f72d9329b64f13c6053125bb9
mibu <mirko.budimir@gmail.com>
2026-01-21 15:53:58 +0100

    fixed data issues and disable ai in ui

--- Semantico.Core.PostgreSql/Data/Migrations/20260119135106_Refactor.cs
+++ Semantico.Core.PostgreSql/Data/Migrations/20260119135106_Refactor.cs
@@ -11,61 +11,6 @@ namespace Semantico.Core.PostgreSql.Data.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropIndex(
-                name: "ix_documentation_versions_created_at",
-                schema: "semantico",
-                table: "documentation_versions");
-
-            migrationBuilder.DropColumn(
-                name: "created_at",
-                schema: "semantico",
-                table: "documentation_versions");
-
-            migrationBuilder.DropColumn(
-                name: "created_at",
-                schema: "semantico",
-                table: "documentation_sections");
-
-            migrationBuilder.DropColumn(
-                name: "modified_at",
-                schema: "semantico",
-                table: "documentation_sections");
-
-            migrationBuilder.DropColumn(
-                name: "created_at",
-                schema: "semantico",
-                table: "data_source_documentations");
-
-            migrationBuilder.DropColumn(
-                name: "last_modified_at",
-                schema: "semantico",
-                table: "data_source_documentations");
-
-            migrationBuilder.DropColumn(
-                name: "modified_at",
-                schema: "semantico",
-                table: "data_source_documentations");
-
-            migrationBuilder.DropColumn(
-                name: "created_at",
-                schema: "semantico",
-                table: "ai_prompt_templates");
-
-            migrationBuilder.DropColumn(
-                name: "modified_at",
-                schema: "semantico",
-                table: "ai_prompt_templates");
-
-            migrationBuilder.DropColumn(
-                name: "created_at",
-                schema: "semantico",
-                table: "ai_alert_configurations");
-
-            migrationBuilder.DropColumn(
-                name: "modified_at",
-                schema: "semantico",
-                table: "ai_alert_configurations");
-
             migrationBuilder.CreateIndex(
                 name: "ix_documentation_versions_created_time",
                 schema: "semantico",
@@ -80,14 +25,6 @@ namespace Semantico.Core.PostgreSql.Data.Migrations
                 name: "ix_documentation_versions_created_time",
                 schema: "semantico",
                 table: "documentation_versions");
-
-            migrationBuilder.AddColumn<DateTime>(
-                name: "created_at",
-                schema: "semantico",
-                table: "documentation_versions",
-                type: "timestamp with time zone",
-                nullable: false,
-                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
 
             migrationBuilder.AddColumn<DateTime>(
                 name: "created_at",
@@ -159,12 +96,6 @@ namespace Semantico.Core.PostgreSql.Data.Migrations
                 type: "timestamp with time zone",
                 nullable: false,
                 defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
-
-            migrationBuilder.CreateIndex(
-                name: "ix_documentation_versions_created_at",
-                schema: "semantico",
-                table: "documentation_versions",
-                column: "created_at");
         }
     }
 }
